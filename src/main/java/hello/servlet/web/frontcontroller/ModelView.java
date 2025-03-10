e26b65f8a205a6443277b260d2f25efebe9c44ed
kittae <jinkitea@naver.com>
2025-03-10 12:42:06 +0900

    [6-3] 뷰 리졸버

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public ModelView(String s) {
+    }
+}
